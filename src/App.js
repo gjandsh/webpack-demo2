e756f54d98e51a143cf2418e9a2dea4be3a44314
gjandsh <gjandsh@126.com>
2017-12-07 13:23:54 +0800

    fix value

--- src/App.js
+++ src/App.js
@@ -9,17 +9,14 @@ class App extends Component {
 	constructor(props){
 			super(props)
 			this.state = {
-					newTodo:'test',
-					todoList:[
-						{id:1,title:'第一个待办'},
-						{id:2,title:'第二个待办'}
-					]
+					newTodo:'',
+					todoList:[]
 				}
 		}
   render() {
 		let todos = this.state.todoList.map((item,index)=>{
 				return (
-					<li>
+					<li key={index}>
 						<TodoItem todo={item}/>
 					</li>
 				)
@@ -28,14 +25,41 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
 				<div className = "inputWrapper">
-					<TodoInput content={this.state.newTodo}/>
+					<TodoInput content={this.state.newTodo}
+					onChange = {this.changeTitle.bind(this)}
+					onSubmit = {this.addTodo.bind(this)}/>
 				</div>
 				<ol>
 					{todos}
 				</ol>
 			</div>
-    );
+    )
   }
+	changeTitle(event){
+		this.setState({
+			newTodo: event.target.value,
+			todoList: this.state.todoList
+		})	
+	}
+	addTodo(event){
+		this.state.todoList.push({
+			id:idMaker(),
+			title:event.target.value,
+			status:null,
+			deleted:false
+		})
+		this.setState({
+			newTodo:'',
+			todoList: this.state.todoList
+		})
+	}
 }
 
 export default App;
+
+let id = 0
+
+function idMaker(){
+	id += 1
+	return id
+}
