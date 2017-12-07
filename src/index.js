b75787df73ecfa18d301b8563ac5060e533de2c7
gjandsh <gjandsh@126.com>
2017-12-07 12:02:28 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome';
 ReactDOM.render( 
-	<h1> Hi, world! < /h1>,
-  document.getElementById('root2')
-);
+< Welcome name="frank"/ > ,
+  document.getElementById('root')
+)
