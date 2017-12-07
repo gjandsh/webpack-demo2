d13141b49f0650a02fdc8e22173f2b3d9324ec8f
gjandsh <gjandsh@126.com>
2017-12-07 13:55:18 +0800

    添加删除

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ class App extends Component {
 		let todos = this.state.todoList.map((item,index)=>{
 				return (
 					<li key={index}>
-						<TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
+						<TodoItem todo={item} 
+							onToggle={this.toggle.bind(this)}
+							onDelete={this.delete.bind(this)}
+						/>
 					</li>
 				)
 			})
@@ -37,6 +40,10 @@ class App extends Component {
   }
 	toggle(e,todo){
 		todo.status = todo.status === 'completed' ?'':'completed'
+		this.setState(this.state)
+	}
+	delete(event,todo){
+		todo.delete = true
 		this.setState(this.state)
 	}
 	changeTitle(event){
